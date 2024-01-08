6aca29ee18437795ad72cf1f393c947ede6c3cdb
Edward <ew010415@gmail.com>
2024-01-08 23:58:25 +0100

    learning tail

---
+++ learningTail/src/SideBar.jsx
@@ -0,0 +1,12 @@
+const SideBar = () =>  {
+    return (
+        <div ClassName="fixed top-0 left-0 h-screen w-16">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+        </div>
+    );
+};
+
+export default SideBar;
---
+++ learningTail/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    mode: 'jit',
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
